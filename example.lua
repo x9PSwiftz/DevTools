ff3a578828ebc5a856a48b416c1d0d1a0bfed47c
9PASA <69302296+x9PSwiftz@users.noreply.github.com>
2023-09-26 20:51:07 +0700

    Delete example.lua

--- example.lua
+++
@@ -1,9 +0,0 @@
-local HttpSpy =  loadstring(game:HttpGet("https://raw.githubusercontent.com/x9PSwiftz/HttpSpyz/main/Init"))({
-    AutoDecode = true, -- Automatically decodes JSON
-    Highlighting = true, -- Highlights the output
-    SaveLogs = true, -- Save logs to a text file
-    CLICommands = true, -- Allows you to input commands into the console
-    ShowResponse = true, -- Shows the request response
-    API = true, -- Enables the script API
-    BlockedURLs = {} -- Blocked urls
-});
