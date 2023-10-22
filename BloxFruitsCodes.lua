b11df4ccde0bb2caadd55a1d888805b8921efa5a
9PASA <69302296+x9PSwiftz@users.noreply.github.com>
2023-10-22 20:38:54 +0700

    Create BloxFruitsCodes.lua

---
+++ BloxFruitsCodes.lua
@@ -0,0 +1,3 @@
+return {
+  "SECRET_ADMIN","ADMIN_TROLL","SUB2CAPTAINMAUI","kittgaming","Sub2Fer999","Enyu_is_Pro","Magicbus","JCWK","Starcodeheo","Bluxxy","fudd10_v2","SUB2GAMERROBOT_EXP1","Sub2NoobMaster123","Sub2Daigrock","Axiore","TantaiGaming","StrawHatMaine","Sub2OfficialNoobie","Fudd10","TheGreatAce","Bignews"
+}
